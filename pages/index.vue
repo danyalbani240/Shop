6a1bd47681ad9aaf4be7efa07d1a1793b78dab81
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-12 11:23:37 +0330

    category finished

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
     <TheNav />
+    <div class="category-container mx-auto">
+      <p class="text-black text-center">دسته بندی محصولات</p>
+      <div class="flex category mx-auto justify-between">
+        <div class="category-item h-full bg-white flex flex-col items-center">
+          <img class="mt-8" src="../assets/images/cake.svg" alt="cake">
+          <p class="text-center mt-6">کیک</p>
+        </div>
+        <div class="category-item h-full bg-white flex flex-col items-center">
+          <img class="mt-8" src="../assets/images/bread.svg" alt="cake">
+          <p class="text-center mt-6">نان</p>
+
+        </div>
+        <div class="category-item h-full bg-white flex flex-col items-center">
+          <img class="mt-8" src="../assets/images/cookies.svg" alt="cake">
+          <p class="text-center mt-6">ساندویچ</p>
+
+        </div>
+        <div class="category-item h-full bg-white flex flex-col items-center">
+          <img class="mt-8" src="../assets/images/cake.svg" alt="cake">
+          <p class="text-center mt-6">شیرینی</p>
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +33,29 @@ export default {
   name: 'IndexPage',
 }
 </script>
+<style scoped>
+.category-container {
+  margin-top: 43px;
+  width: 872px;
+}
+.category-container p {
+  font-size: 32px;
+  line-height: 32px;
+}
+.category {
+  width: 872px;
+  margin-top: 26px;
+  height: 160px;
+}
+.category-item {
+  width: 194px;
+  box-shadow: 0px 24px 50px rgba(0, 0, 0, 0.1);
+  border-radius: 24px;
+  cursor: pointer;
+}
+.category-item p{
+  font-size: 24px;
+line-height: 24px;
+
+}
+</style>
