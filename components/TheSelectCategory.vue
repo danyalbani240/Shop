a87087fceb2d6a9ee42bcc461be966c9805cfd4a
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-12 12:10:56 +0330

    Select Category is Done.✅

--- components/TheSelectCategory.vue
+++ components/TheSelectCategory.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="select-category mx-auto rounded-3xl"></div>
+  <div
+    class="select-category mx-auto rounded-3xl mt-20 flex items-center justify-between"
+    dir="rtl"
+  >
+    <div class="select flex">
+      <p class="text-black text-lg">انتخاب دسته بندی</p>
+      <BaseSelect style="margin-right:18px;" :options="['شیرینی']" />    
+    </div>
+    <BaseSearch placeholder="جستجو" style="margin-left:13px;" />
+  </div>
 </template>
 
 <script>
+
 export default {}
 </script>
 
@@ -12,5 +22,16 @@ export default {}
   background-color: #e5e5e5;
   box-shadow: 0px 24px 50px rgba(0, 0, 0, 0.1);
   height: 60px;
+}
+.select {
+  font-size: 24px;
+  line-height: 24px;
+}
+.select p {
+    margin-right: 22px;
+}
+.search {
+    border-radius: 24px;
+    
 }
 </style>
