740d8be693a77a9b3d0d3f8df941c633e0317883
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-12 11:26:04 +0330

    base styles added

---
+++ components/TheSelectCategory.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="select-category mx-auto rounded-3xl"></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.select-category {
+  width: 1180px;
+  background-color: #e5e5e5;
+  box-shadow: 0px 24px 50px rgba(0, 0, 0, 0.1);
+  height: 60px;
+}
+</style>
