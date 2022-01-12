4fac8fab6291e608e81cf233183f26acb477ba56
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-12 12:15:04 +0330

    component Created

---
+++ components/TheBesSellProducts.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="best-products-container mx-auto mt-20">
+
+      <p class="text-3xl text-center text-black">خرید محصولات پر فروش</p>
+    <div class="best-products"></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.best-products-container {
+  width: 1180px;
+  height: 327px;    
+}
+</style>
