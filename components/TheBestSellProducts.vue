d598b37560e9765502faffc74a423db3c9f18ff5
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-12 12:21:49 +0330

    Base Styles Added

--- components/TheBestSellProducts.vue
+++ components/TheBestSellProducts.vue
@@ -2,7 +2,7 @@
   <div class="best-products-container mx-auto mt-20">
 
       <p class="text-3xl text-center text-black">خرید محصولات پر فروش</p>
-    <div class="best-products"></div>
+    <div class="best-products flex items-end justify-between"></div>
   </div>
 </template>
 
@@ -14,5 +14,10 @@ export default {}
 .best-products-container {
   width: 1180px;
   height: 327px;    
+}
+.best-products{
+  background-color: #e5e5e5;
+  width: 100%;
+  height: 327px;
 }
 </style>
