515e7e9da9acbc4201b2767f2144a50942bff958
DanyalBani <mr.d.bani.d@gmail.com>
2022-01-12 13:38:59 +0330

    Done ✅

--- components/TheBestSellProducts.vue
+++ components/TheBestSellProducts.vue
@@ -2,6 +2,9 @@
   <div class="best-products-container mx-auto mt-20">
 
       <p class="text-3xl text-center text-black">خرید محصولات پر فروش</p>
+      <div class="flex">
+        <img style="margin-right:14px;" class="cursor-pointer" src="../assets/images/leftarrow.svg" alt=""><img src="../assets/images/rightarrow.svg" alt="">
+      </div>
     <div class="best-products flex items-end justify-between">
       <BaseItem />
       <BaseItem :discount="2" />
